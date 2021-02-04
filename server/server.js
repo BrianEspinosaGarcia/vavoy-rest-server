e36155954c171d774d04f027819de6e7763472bf
edgaryoset.miranda <edgaryoset.miranda@bbva.com>
2021-02-04 14:14:43 -0600

    solved cors

--- server/server.js
+++ server/server.js
@@ -7,17 +7,14 @@ const bodyParser = require('body-parser');
 
 /* Set Middlewares: */
 
-app.use( bodyParser.urlencoded( { extended: false }));
 app.use(bodyParser.json());
-app.use( require('./routes/traveler') );
 app.use( cors() );
-app.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
+app.use( require('./routes/traveler') );
 
 mongoose.connect( process.env.NODE_URLDB , 
                   {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology: true},
